f2e8721a0abcad3d1bb9b05cf14a55b19ec0d2cd
Antony rossi de jesus <antonyrossidejesus@gmail.com>
2024-08-02 23:52:43 -0300

    add07

--- js/acordeao.js
+++ js/acordeao.js
@@ -1,16 +1,49 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   const acordeaoItems = document.querySelectorAll(".item-acordeao");
+
+//   acordeaoItems.forEach((item) => {
+//     const header = item.querySelector(".cabecalho-item");
+
+//     header.addEventListener("click", () => {
+//       const isOpen = item.classList.contains("aberto");
+//       const isOpenImg = item.classList.contains("img-top");
+
+//       acordeaoItems.forEach((el) => el.classList.remove("aberto"));
+
+//       if (!isOpen) {
+//         item.classList.add("aberto");
+//       }
+
+//       if (!isOpenImg) {
+//         item.classList.add("img-top");
+//       }
+//     });
+//   });
+// });
+
 document.addEventListener("DOMContentLoaded", () => {
   const acordeaoItems = document.querySelectorAll(".item-acordeao");
 
   acordeaoItems.forEach((item) => {
     const header = item.querySelector(".cabecalho-item");
+    const imgSeta = header.querySelector(".img-seta");
 
     header.addEventListener("click", () => {
       const isOpen = item.classList.contains("aberto");
 
-      acordeaoItems.forEach((el) => el.classList.remove("aberto"));
+      acordeaoItems.forEach((el) => {
+        el.classList.remove("aberto");
+        const elImgSeta = el.querySelector(".img-seta");
+        if (elImgSeta) {
+          elImgSeta.src = "/public/seta-para-baixo.png";
+        }
+      });
 
       if (!isOpen) {
         item.classList.add("aberto");
+        imgSeta.src = "/public/seta-para-cima.png";
+      } else {
+        imgSeta.src = "/public/seta-para-baixo.png";
       }
     });
   });
